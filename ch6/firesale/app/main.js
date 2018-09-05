7eea169136144a400777abb108d224306b4d5b8d
Bartosz Białas <belamenso@protonmail.ch>
2018-09-05 19:59:37 +0200

    Finished chapter 6

---
+++ ch6/firesale/app/main.js
@@ -0,0 +1,132 @@
+const { app, BrowserWindow, dialog } = require('electron');
+const fs = require('fs')
+
+let mainWindow = null;
+const windows = new Set()
+const openFiles = new Map()
+
+app.on('ready', () => createWindow())
+
+const getFileFromUser = exports.getFileFromUser = (targetWindow) => {
+    const files = dialog.showOpenDialog(targetWindow, {
+        properties: ['openFile'],
+        filters: [
+            { name: 'Text Files', extensions: ['txt'] },
+            { name: 'Markdown Files', extensions: ['md', 'markdown'] }
+        ]
+    });
+
+    if (files) openFile(targetWindow, files[0])
+}
+
+const openFile = (targetWindow, file) => {
+    const content = fs.readFileSync(file).toString()
+    app.addRecentDocument(file)
+    targetWindow.setRepresentedFilename(file)
+    targetWindow.webContents.send('file-opened', file, content)
+}
+
+let createWindow = exports.createWindow = () => {
+    let x, y
+
+    const currentWindow = BrowserWindow.getFocusedWindow()
+
+    if (currentWindow) {
+        const [ currentWindowX, currentWindowY ] = currentWindow.getPosition()
+        x = currentWindowX + 10
+        y = currentWindowY + 10
+    }
+
+    let newWindow = new BrowserWindow({ x, y, show: false })
+
+    newWindow.loadURL(`file://${__dirname}/index.html`)
+
+    newWindow.once('ready-to-show', () => newWindow.show())
+
+    newWindow.on('closed', () => {
+        windows.delete(newWindow)
+        stopWatchingFile(newWindow)
+        newWindow = null
+    })
+
+    newWindow.on('close', (event) => {
+        if (newWindow.isDocumentEdited()) {
+            event.preventDefault()
+
+            const result = dialog.showMessageBox(newWindow, {
+                type: "warning",
+                title: "Quit with unsaved changes?",
+                message: "Your changes will be lost",
+                buttons: [ 'Quit', 'Cancel' ],
+                deafultId: 0,
+                cancelId: 1
+            })
+
+            if (result === 0) newWindow.destroy()
+        }
+    })
+
+    windows.add(newWindow)
+    return newWindow
+}
+
+app.on('will-finish-launching', () => {
+    app.on('open-file', (event, file) => {
+        const win = createWindow()
+        win.once('ready-to-show', () => {
+            openWith(win, file)
+        })
+    })
+})
+
+const saveHTML = exports.saveHTML = (targetWindow, content) => {
+    const file = dialog.showSaveDialog(targetWindow, {
+        title: 'Save HTML',
+        defaultPath: app.getPath('documents'),
+        filters: [
+            { name: 'HTML Files', extensions: ['html', 'htm'] }
+        ]
+    })
+
+    if (!file) return;
+
+    fs.writeFileSync(file, content)
+}
+
+const saveMarkdown = exports.saveMarkdown = (targetWindow, file, content) => {
+    if (!file){
+        file = dialog.showSaveDialog(targetWindow, {
+            title: 'Save Markdown',
+            defaultPath: app.getPath('documents'),
+            filters: [
+                { name: 'Markdown Files', extentions: ['md', 'markdown'] }
+            ]
+        })
+    }
+
+    if (!file) return;
+
+    fs.writeFileSync(file, content)
+    openFile(targetWindow, file)
+}
+
+const startWatchingFile = (targetWindow, file) => {
+    stopWatchingFile(targetWindow)
+
+    const watcher = fs.watchFile(file, (event) => {
+        if (event === 'change') {
+            const content = fs.readFileSync(file)
+            targetWindow.webContents.send('file-changed', file, content)
+        }
+    })
+
+    openFiles.set(targetWindow, watcher)
+}
+
+const stopWatchingFile = (targetWindow) => {
+    if (openFiles.has(targetWindow)) {
+        openFiles.get(targetWindow).stop()
+        openFiles.delete(targetWindow)
+    }
+}
+
---
+++ ch6/firesale/app/renderer.js
@@ -0,0 +1,145 @@
+const marked = require('marked')
+const { remote, ipcRenderer } = require('electron')
+const mainProcess = remote.require('./main.js')
+const path = require('path')
+
+let filePath = null
+let originalContent = ''
+const currentWindow = remote.getCurrentWindow()
+
+const markdownView = document.querySelector('#markdown');
+const htmlView = document.querySelector('#html');
+const newFileButton = document.querySelector('#new-file');
+const openFileButton = document.querySelector('#open-file');
+const saveMarkdownButton = document.querySelector('#save-markdown');
+const revertButton = document.querySelector('#revert');
+const saveHtmlButton = document.querySelector('#save-html');
+const showFileButton = document.querySelector('#show-file');
+const openInDefaultButton = document.querySelector('#open-in-default');
+
+const renderMarkdownToHtml = (markdown) => {
+	htmlView.innerHTML = marked(markdown, { sanitize: true });
+};
+
+markdownView.addEventListener('keyup', (event) => {
+	const currentContent = event.target.value;
+	renderMarkdownToHtml(currentContent);
+});
+
+openFileButton.addEventListener('click', () => {
+    mainProcess.getFileFromUser(currentWindow)
+})
+
+ipcRenderer.on('file-opened', (event, file, content) => {
+    filePath = file
+    originalContent = content
+
+    markdownView.value = content
+    renderMarkdownToHtml(content)
+
+    updateUserInterface()
+})
+
+newFileButton.addEventListener('click', () => {
+    mainProcess.createWindow()
+})
+
+const updateUserInterface = (isEdited) => {
+    let title = 'Fire Sale'
+
+    if (filePath) title = `${path.basename(filePath)} - ${title}`
+    if (isEdited) title = `${title} (Edited)`
+
+    currentWindow.setTitle(title)
+
+    saveMarkdownButton.disabled = !isEdited
+    revertButton.disabled = !isEdited
+}
+
+markdownView.addEventListener('keyup', (event) => {
+    const currentContent = event.target.value
+    renderMarkdownToHtml(currentContent)
+    updateUserInterface(currentContent !== originalContent)
+})
+
+markdownView.addEventListener('dragover', (event) => {
+    const file = getDraggedFile(event);
+
+    if (fileTypeIsSupported(file)) {
+        markdownView.classList.add('drag-over'); //
+    } else {
+        markdownView.classList.add('drag-error'); //
+    }
+});
+
+markdownView.addEventListener('dragleave', () => { //
+    markdownView.classList.remove('drag-over');
+    markdownView.classList.remove('drag-error');
+});
+
+saveHtmlButton.addEventListener('click', () => {
+    mainProcess.saveHTML(currentWindow, htmlView.innerHTML)
+})
+
+saveMarkdownButton.addEventListener('click', () => {
+    mainProcess.saveMarkdown(currentWindow, filePath, markdownView.value)
+})
+
+revertButton.addEventListener('click', () => {
+    markdownView.value = originalContent
+    renderMarkdownToHtml(originalContent)
+    updateUserInterface()
+})
+
+const getDraggedFile = (event) => {
+    return event.dataTransfer.files[0]
+}
+
+const fileTypeIsSupported = (file) => {
+    return ['text/plain', 'text/markdown'].includes(file.type) 
+}
+
+document.addEventListener('dragstart', event => event.preventDefault())
+document.addEventListener('dragover', event => event.preventDefault())
+document.addEventListener('dragleave', event => event.preventDefault())
+document.addEventListener('drop', event => event.preventDefault())
+
+const renderFile = (file, content) => {
+    filePath = file
+    originalContent = content
+
+    markdownView.value = content
+    renderMarkdownToHtml(content)
+
+    updateUserInterface()
+}
+
+ipcRenderer.on('file-opened', (event, file, content) => {
+    if (currentWindow.isDocumentEdited()) {
+        const result = remote.dialog.showMessageBox(currentWindow, {
+            type: 'warning',
+            title: 'Overwrite Current Unsaved Changes?',
+            message: 'Opening a new file in this window will overwrite your unsaved changes. Open this file anyway?',
+            buttons: [ 'Yes', 'Cancel' ],
+            defaultId: 0,
+            cancelId: 1
+        });
+
+        if (result === 1) { return; } 
+    }
+
+    renderFile(file, content); 
+});
+
+ipcRenderer.on('file-changed', (event, file, content) => {
+    const result = remote.dialog.showMessageBox(currentWindow, {
+        type: 'warning',
+        title: 'Overwrite Current Unsaved Changes?',
+        message: 'Another application has changed this file. Load changes?',
+        buttons: [ 'Yes', 'Cancel' ],
+        defaultId: 0,
+        cancelId: 1
+    });
+
+    renderFile(file, content);
+});
